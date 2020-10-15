e3128a4ed6215463a37420145a1bdb49a75aa060
gempellm <lev_gempel@inbox.ru>
2020-10-15 19:54:03 +0300

    The basis

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import {render} from 'react-dom'
+import './index.css'
 
 function App() {
     return (
         <div>
-            <h2>h2</h2>
+            <Header />
+            <Main />
+            <CreatePost />
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div className="header">
+            <ul>
+                <a href="#"><li>Главная</li></a>
+                <a href="#"><li>О проекте</li></a>
+                <a href="#"><li>Регистрация</li></a>
+                <a href="#"><li>Вход</li></a>
+            </ul>
+        </div>
+    )
+}
+
+function Main() {
+    return (
+        <div className="main" id="main">
+            <p>main</p>
+        </div>
+    )
+}
+
+function CreatePost() {
+    return (
+        <div className="createPost">
+            <form>
+                <input type="text" id="topic" name="topic"></input>
+                <label for="topic">topic</label>
+                <textarea id="essence" name="essence"></textarea>
+                <label for="essence">essence</label>
+                <button>Отправить</button>
+            </form>
         </div>
     )
 }
